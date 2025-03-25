53a5d10053c1d2982ef96fc849e13b946e1ed0c7
Fernando Romero <fernando.romero@road.io>
2025-03-25 21:46:18 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { AccountRepo } from "./account-repo.ts";
 import { InvoiceRepo } from "./invoice-repo.ts";
 import { InvoiceService } from "./invoice-service.ts";
 import { makeClient } from "lidex";
+import { MongoPersistence } from "lidex-mongo";
 
 const app = express();
 app.use(express.json());
@@ -19,13 +20,14 @@ handlers.set(
   invoiceService.collectPayment.bind(invoiceService),
 );
 
-const client = await makeClient({
-  handlers,
-  mongoUrl: "mongodb://localhost:27017/lidex?directConnection=true",
-  now: () => new Date(),
-});
+const persistence = new MongoPersistence(
+  "mongodb://localhost:27017/lidex?directConnection=true",
+);
 
-app.post("/invoices/collect-all", async (req, res) => {
+await persistence.init();
+const client = await makeClient({ handlers, persistence });
+
+app.post("/collect", async (req, res) => {
   const pool: Promise<boolean>[] = [];
 
   for (let i = 0; i < 100_000; i++) {
@@ -44,20 +46,8 @@ app.post("/invoices/collect-all", async (req, res) => {
   res.send();
 });
 
-app.post("/invoices/:invoiceId/collect", async (req, res) => {
-  const invoiceId = req.params.invoiceId;
-
-  await client.start(
-    `collect-payment-${invoiceId}`,
-    "collect-payment",
-    invoiceId,
-  );
-
-  res.send();
-});
-
 const server = app.listen(port, () => {
-  console.log(`durex app listening on port ${port}`);
+  console.log(`lidex example app listening on port ${port}`);
 });
 
 let stop = false;
